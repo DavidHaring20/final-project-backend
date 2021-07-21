5d0e7cb7b7c15a00defe427621fe574c72d243a2
Josipa <jraspor111@gmail.com>
2021-07-21 09:10:42 +0200

    Route name change

--- routes/api.php
+++ routes/api.php
@@ -27,7 +27,7 @@ Route::get('/export-json/restaurant/{id}', [FileExportController::class, 'FileEx
 
 //Restaurant
 Route::get('/restaurants', [RestaurantController::class, 'show']);
-Route::get('/restaurant/{id}', [RestaurantController::class, 'index']);
+Route::get('/restaurant/show/{id}', [RestaurantController::class, 'index']);
 Route::get('/restaurant/{id}', [RestaurantController::class, 'destroy']);
 
 //Category

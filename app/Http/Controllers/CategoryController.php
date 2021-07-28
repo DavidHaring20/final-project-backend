a93d85b6a5986dbcf169bc2cbe428929a781eeaa
Josipa <jraspor111@gmail.com>
2021-07-28 11:07:49 +0200

    DB Transactions Category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -6,24 +6,35 @@ use App\Models\CategoriesTranslation;
 use App\Models\Category;
 use App\Models\Restaurant;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Throwable;
 
 class CategoryController extends Controller
 {
     public function store($id, Request $request) {
 
         $restaurant = Restaurant::findOrFail($id);
-
-        $newCategory = $restaurant->categories()->create([
-            'position' => 1,
-        ]);
-
         $translations = collect(json_decode($request->translations));
-        foreach ($translations as $language_code => $value) {
-            $newCategory->translations()->create([
-                'language_code' => $language_code,
-                'is_default' => false,
-                'name' => $value,
+
+        try {
+            DB::beginTransaction();
+
+            $newCategory = $restaurant->categories()->create([
+                'position' => 1,
             ]);
+
+            foreach ($translations as $language_code => $value) {
+                $newCategory->translations()->create([
+                    'language_code' => $language_code,
+                    'is_default' => false,
+                    'name' => $value,
+                ]);
+            }
+
+            DB::commit();
+        } catch(Throwable $e) {
+            DB::rollBack();
+            report($e);
         }
 
         $newCategory = Category::with('translations')->find($newCategory->id);
@@ -42,9 +53,18 @@ class CategoryController extends Controller
         $category_translations = CategoriesTranslation::where('category_id', $id)->get();
         $translations = collect(json_decode($request->translations));
 
-        foreach($category_translations as $category_translation) {
-            $category_translation->name = $translations[$category_translation->language_code];
-            $category_translation->save();
+        try {
+            DB::beginTransaction();
+
+            foreach($category_translations as $category_translation) {
+                $category_translation->name = $translations[$category_translation->language_code];
+                $category_translation->save();
+            }
+
+            DB::commit();
+        } catch(Throwable $e) {
+            DB::rollBack();
+            report($e);
         }
 
         $updatedCategory = Category::with(

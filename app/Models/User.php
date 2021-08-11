5f8e796894c36f1fbb5c59e1ff688e205ef29261
David <davidharingri@gmail.com>
2021-08-11 16:19:07 +0200

    Removed timestamps in the User. Added migration for users_table and migration for add_subtitle_to_item_translations.

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,6 +19,7 @@ class User extends Authenticatable
     protected $table = 'users';
     protected $primaryKey = 'id';
     public $incrementing = 'true';
+    public $timestaps = 'false';
     
     protected $fillable = [
         'name',

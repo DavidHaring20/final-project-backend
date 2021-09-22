a0b5033b2253e2536d9ab5d7ede218a886f71463
David <davidharingri@gmail.com>
2021-09-22 13:23:11 +0200

    User can Upload Picture

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -10,34 +10,63 @@ use Validator;
 
 class FileController extends Controller
 {
-    public function getPresignedUrl(Request $request) {
-        $validator = Validator::make(
-            $request -> all(),
+    // public function getPresignedUrl(Request $request) {
+    //     $validator = Validator::make(
+    //         $request -> all(),
+    //         [
+    //             'userId' => 'required',
+    //         ],
+    //         [],
+    //         []
+    //     );
+
+    //     if ($validator -> fails()) {
+    //         return response() -> json(
+    //             [
+    //                 'message' => 'Error. Wrong User.'
+    //             ], 400
+    //         );
+    //     }
+
+    //     $data = $validator -> valid();
+    //     User::findOrFail($data['userId']);
+    //     $path = 'pictures/'.Str::random(9);
+
+
+    //     $presignedUrl = Storage::disk('minio') -> temporaryUrl($path, '+5 minutes');
+
+    //     return response() -> json(
+    //         [
+    //             'presignedUrl' => $presignedUrl
+    //         ]
+    //     );
+    // }
+
+    public function putPicture(Request $request) {
+
+        $validator = Validator::make($request -> all(), 
             [
-                'userId' => 'required',
+                'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
             ],
             [],
             []
         );
 
         if ($validator -> fails()) {
-            return response() -> jsonFail(
+            return response() -> json(
                 [
-                    'message' => 'Error. Wrong User.'
-                ], 400
-            );
+                    'errorMessage' => 'Error'
+                ]
+            ); 
         }
 
-        $data = $validator -> valid();
-        User::findOrFail($data['userId']);
-        $path = 'pictures/'.Str::random(9);
-
-
-        $presignedUrl = Storage::disk('minio') -> temporaryUrl($path, '+5 minutes');
+        $picture = $request -> image;
+         
+        $path = Storage::disk('minio') -> putFile('photos', $picture);
 
         return response() -> json(
             [
-                'presignedUrl' => $presignedUrl
+                'path' => $path
             ]
         );
     }
